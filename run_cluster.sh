6698a914c91b382c67cdf369f899c45aae30662e
Guy Allard <w.g.allard@lumc.nl>
2017-07-20 11:48:32 +0200

    additional parameters for run script

--- run_cluster.sh
+++ run_cluster.sh
@@ -1,15 +1,29 @@
 #! /bin/bash
 
-OUTPUT_DIR=pipeline_output
+OUTPUT_DIR=""
+EXTRA_CONFIG=""
+CONFIG_FILE=""
 
-while getopts ":d:" opt; do
+while getopts ":d:c:e:" opt; do
     case $opt in
         d)
-            OUTPUT_DIR=$OPTARG
-            echo "Writing pipeline output to $OUTPUT_DIR" >&2
+            OUTPUT_DIR="--directory $OPTARG"
+            echo "Writing pipeline output to $OPTARG" >&2
+            ;;
+        c)
+            CONFIG_FILE="--configfile $OPTARG"
+            echo "Using additional config file $OPTARG"
+            ;;
+        e)
+            EXTRA_CONFIG="--config ${OPTARG[@]}"
+            echo "Using config options ${OPTARG[@]}" >&2
             ;;
         \?)
             echo "Invalid option: -$OPTARG" >&2
+            echo "Valid options are:" >&2
+            echo "  -d : output directory" >&2
+            echo "  -c : config file" >&2
+            echo "  -e : extra config values" >&2
             exit 1
             ;;
         :)
@@ -19,12 +33,13 @@ while getopts ":d:" opt; do
     esac
 done
 
-snakemake --latency-wait 90 \
+snakemake $OUTPUT_DIR \
+          $EXTRA_CONFIG \
+          $CONFIG_FILE \
+          --latency-wait 90 \
           --drmaa ' -N preprocessor -pe BWA {cluster.threads} -l h_vmem={cluster.vmem} -q all.q -cwd -V -j Y' \
           --drmaa-log-dir cluster_logs \
           --jobs 100 \
           --max-jobs-per-second 10 \
-          --cluster-config cluster_settings.yaml \
-          --directory $OUTPUT_DIR
-
+          --cluster-config cluster_settings.yaml
 
