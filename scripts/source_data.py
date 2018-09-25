1b24004a0fca5e7e88cbeb59fd1365b14b397237
Guy Allard <w.g.allard@lumc.nl>
2018-09-25 11:32:20 +0200

    fix namespace

--- scripts/source_data.py
+++ scripts/source_data.py
@@ -1,10 +1,15 @@
+from snakemake import shell
+
 config = snakemake.config
 wildcards = snakemake.wildcards
+basecalls = snakemake.input
+subreads = snakemake.output[0]
+scraps = snakemake.output[1]
 
 if config["SEQUENCING_PLATFORM"] == "RS2":
-    shell("cd preprocessor/basecalls && bax2bam -o {wildcards.moviename} {input}")
+    shell("cd preprocessor/basecalls && bax2bam -o {wildcards.moviename} {basecalls}")
 else:
     subreads = next(f for f in input if f.endswith("subreads.bam"))
     scraps = next(f for f in input if f.endswith("scraps.bam"))
-    shell("ln -s {subreads} {output[0]}")
-    shell("ln -s {scraps}   {output[1]}")
+    shell("ln -s {subreads} {subreads}")
+    shell("ln -s {scraps}   {scraps}")
